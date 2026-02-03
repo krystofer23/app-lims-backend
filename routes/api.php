0d85935470821ec1a803f3410cf36d1c1d426eba
krystofer23 <141861272+krystofer23@users.noreply.github.com>
2026-02-02 22:38:03 -0500

    routing

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,16 @@
 <?php
 
 use App\Http\Controllers\CompanyApiController;
-use App\Http\Middleware\JWTMiddleware; 
-use Illuminate\Http\Request;
+use App\Http\Middleware\JWTMiddleware;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([JWTMiddleware::class])->prefix('company')->controller(CompanyApiController::class)->group(function () {
-    
-    Route::get('', 'index');     
-    Route::post('', 'store');         
-    Route::get('{id}', 'show');       
-    Route::put('{id}', 'update');     
-    Route::delete('{id}', 'destroy'); 
+Route::middleware(JWTMiddleware::class)->prefix('v1')->group(function () {
 
+    Route::controller(CompanyApiController::class)->prefix('company')->group(function () {
+        Route::get('', 'index');
+        Route::get('{id}', 'show');
+        Route::post('', 'store');
+        Route::put('{id}', 'update');
+        Route::delete('{id}', 'destroy');
+    });
 });
